8797ffe4708519f82901b69fa280a37ba76f1099
descoplay <eu@diasrafael.com.br>
2019-03-15 14:30:36 -0300

    manipulando a sequencia de perguntas

--- demo.js
+++ demo.js
@@ -6,8 +6,30 @@ const perguntas = {
     signo: 'Qual é o seu signo?',
 }
 
-userInput(perguntas).then(respostas => {
-    console.log(respostas.nome)
-    console.log(respostas.idade)
-    console.log(respostas.signo)
+const tratamentoDeResposta = (nomePergunta, resposta) => {
+    switch (nomePergunta) {
+        case 'idade': {
+            if (parseInt(resposta) < 18) {
+                return Promise.reject({
+                    message: 'Idade não pode ser menor que 18',
+                    type: 'break',
+                })
+            }
+        }
+            break
+        case 'signo': {
+            if ([ 'Câncer', 'Capricórneo', 'Libra', 'Gêmeos', ].indexOf(resposta) === -1) {
+                return Promise.reject({
+                    message: 'Signo inválido',
+                    type: 'repeat',
+                })
+            }
+        }
+    }
+
+    return Promise.resolve()
+}
+
+userInput(perguntas, tratamentoDeResposta).then(respostas => {
+    console.log(respostas)
 })
--- index.js
+++ index.js
@@ -2,25 +2,41 @@ const readline = require('readline')
 
 const promiseLoopArray = require('@desco/promise-loop-array')
 
-const userInput = _questions => {
+const userInput = (_questions, _treatment) => {
     const questions = Object.values(_questions)
     const names = Object.keys(_questions)
+    const treatment = _treatment ? _treatment : () => Promise.resolve()
 
     return promiseLoopArray(questions, (question, index) => {
-        return new Promise(resolve => {
+        return new Promise((resolve, reject) => {
             const Interface = readline.createInterface({
                 input: process.stdin,
                 output: process.stdout,
             })
 
             Interface.question(`${question} `, answer => {
-                resolve({ name: names[index], answer, })
+                return treatment(names[index], answer)
+                    .then(() => {
+                        resolve({ name: names[index], answer, })
 
-                Interface.close()
+                        Interface.close()
+                    })
+                    .catch(data => {
+                        console.log(data.message)
+
+                        Interface.close()
+
+                        if (data.type === 'repeat') {
+                            reject({ result: question, type: data.type, })
+                        }
+                        else {
+                            reject({ result: { name: names[index], answer, }, type: data.type, })
+                        }
+                    })
             })
         })
     }).then(responses => {
-        const toReturn = { ..._questions, }
+        const toReturn = {}
 
         responses.map(response => {
             toReturn[response.name] = response.answer
